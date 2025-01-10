60aaa69c6048d43316dd7bc13d76f19da5098832
adam.zowde <adam.zowde@runi.ac.il>
2025-01-10 12:37:05 +0200

    check q4

--- logic.js
+++ logic.js
@@ -19,45 +19,31 @@ function listen() {
     document.getElementById("myform").addEventListener("click", validation_fields);
 }
 
-// Validation function for the form fields
 function validation_fields(event) {
     // Prevent form submission to handle validation first
     event.preventDefault();
-    // Access the value of the username input field
-    const username = document.getElementById('username').value;
 
-    // Regular expression for validating the username
+    const username = document.getElementById('username').value;
     const usernameRegex = /^[a-zA-Z0-9-]{4,}$/;
 
     const password = document.getElementById('password').value;
-        // Regular expression for validating the password
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*-_()]).+$/;
-            // Regular expression for validating the email
+
     const nothashtaganddots = /^(?!.*#)(?!.*[.]{2}).*$/;
-    const regex = /^[^.-][A-Za-z0-9%&+*#$^!()_{}|?><=~;,.]*@[A-Za-z0-9-]+\.[A-Za-z]{2,}$/;
+    const regex = /^[a-zA-Z0-9%&+*#$^!()_{}|?><=~;,.]+@[A-Za-z0-9-]+\.[A-Za-z]{2,}$/;
     const email = document.getElementById("email").value;
-   if(document.getElementById("age").value <10 || document.getElementById("age").value > 120)
-   {
-    alert("The form is invalid");
-   }
-   else if(!usernameRegex.test(username))
-   {
-    alert("The form is invalid");
 
-   }
-   else if(!passwordRegex.test(password)){
-    alert("The form is invalid");
-
-   }
-   else if(!nothashtaganddots.test(email) || !regex.test(email)){
-    alert("The form is invalid");
-
-   }
-   else
-   {
-    alert("The form is valid");
-   }
-
+    if (document.getElementById("age").value < 10 || document.getElementById("age").value > 120) {
+        alert("The form is invalid");
+    } else if (!usernameRegex.test(username)) {
+        alert("The form is invalid");
+    } else if (!passwordRegex.test(password)) {
+        alert("The form is invalid");
+    } else if (!nothashtaganddots.test(email) || !regex.test(email)) {
+        alert("The form is invalid");
+    } else {
+        alert("The form is valid");
+    }
 }
 // Initialize the event listener when the page loads
 window.onload = listen;
